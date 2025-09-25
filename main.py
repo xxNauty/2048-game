4235beb0e09a56b03b01db441dcffc221e628563
Mateusz <mateusz2003w@gmail.com>
2025-09-25 11:46:03 +0200

    working menu, play game button, quit button

--- main.py
+++ main.py
@@ -23,6 +23,60 @@ game_history_content.pack(fill="both", expand=True)
 main_menu_frame = tk.Frame(container)
 main_menu_frame.grid(row=0, column=0, sticky="nsew")
 
+title_label = tk.Label(
+    master=main_menu_frame,
+    text="2048",
+    font=("Verdana", 30, "bold"),
+    justify="center"
+)
+title_label.pack(pady=10)
+
+def play_game():
+    game_frame.tkraise()
+
+play_button = tk.Button(
+    master=main_menu_frame,
+    text="New game",
+    font=("Verdana", 20, "normal"),
+    justify="center",
+    width=10,
+    command=play_game
+)
+play_button.pack(pady=10)
+
+history_button = tk.Button(
+    master=main_menu_frame,
+    text="Last games",
+    font=("Verdana", 20, "normal"),
+    justify="center",
+    width=10,
+    state="disabled"
+)
+history_button.pack(pady=10)
+
+settings_button = tk.Button(
+    master=main_menu_frame,
+    text="Settings",
+    font=("Verdana", 20, "normal"),
+    justify="center",
+    width=10,
+    state="disabled"
+)
+settings_button.pack(pady=10)
+
+def quit_game():
+    root.destroy()
+
+end_button = tk.Button(
+    master=main_menu_frame,
+    text="Quit game",
+    font=("Verdana", 20, "normal"),
+    justify="center",
+    width=10,
+    command=quit_game
+)
+end_button.pack(pady=10)
+
 records_frame = tk.Frame(container)
 records_frame.grid(row=0, column=0, sticky="nsew")
 
@@ -30,6 +84,5 @@ results_frame = tk.Frame(container)
 results_frame.grid(row=0, column=0, sticky="nsew")
 
 if __name__ == "__main__":
-    # game_history_frame.tkraise()
-    game_frame.tkraise()
+    main_menu_frame.tkraise()
     root.mainloop()
